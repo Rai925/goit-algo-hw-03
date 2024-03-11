73b20a1428158af76489e01e0d496457ce0c8afe
Rai925 <154880397+Rai925@users.noreply.github.com>
2024-03-11 12:27:28 +0200

    Update task-4.py

--- task-4.py
+++ task-4.py
@@ -4,22 +4,21 @@ users = [
     {"name": "Anne Stuart", "birthday": "1665.02.06"},
     {"name": "Clark Gable", "birthday": "1901.02.01"},
     {"name": "John Joseph Travolta", "birthday": "1954.02.18"},
-    {"name": "Dwayne Douglas Johnson", "birthday": "1972.05.02"}
+    {"name": "Dwayne Douglas Johnson", "birthday": "1972.05.10"},
+    {"name": "Taras Shevchenko", "birthday": "1814.03.09"},
+    {"name": "Carlos Ray Norris", "birthday": "1940.03.10"}
 ]
 
 def get_upcoming_birthdays(users=None):
-    tdate = datetime.today().date()
+    today = datetime.today().date()
     upcoming_birthdays = []
-
     for user in users:
-        bdate = datetime.strptime(f"{tdate.year}-{user['birthday'][5:]}", "%Y-%m.%d").date()
-        days_between = (bdate - tdate).days
-        week_day = bdate.isoweekday()
-
-        if 0 <= days_between < 7 and (week_day < 6 or (bdate + timedelta(days=(1 if week_day == 6 else 2))).weekday() == 0):
-            formatted_birthday = bdate.strftime("%d %B").replace(" 0", " ")
+        bdate = datetime.strptime(user['birthday'], "%Y.%m.%d").date().replace(year=today.year)
+        if 0 <= (bdate - today).days < 7:
+            if bdate.weekday() in [5, 6]:
+                days_until_monday = (7 - bdate.weekday()) % 7
+                bdate += timedelta(days=days_until_monday)
+            formatted_birthday = bdate.strftime("%d %B")
             upcoming_birthdays.append(f"{user['name']} {formatted_birthday}")
-
     return upcoming_birthdays
-
 print(get_upcoming_birthdays(users))
